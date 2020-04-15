7f3004030b76395ca542166814c27481278857b7
kbutlerbasham <52687572+kbutlerbasham@users.noreply.github.com>
2020-04-14 21:53:49 -0700

    added routing and code for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {RecipesListComponent} from './recipes-list/recipes-list.component'
-import {SignInComponent} from './sign-in/sign-in.component'
+import {RecipesListComponent} from './recipes-list/recipes-list.component';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+
 
 
 const routes: Routes = [
   {path: 'recipes', component: RecipesListComponent},
   {path: 'login', component: SignInComponent},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
