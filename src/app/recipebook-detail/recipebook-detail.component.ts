e588cd3743c9ad75ee269f2764fbea6577482188
crooksdmrs <david.crooks@daveramsey.com>
2020-04-22 10:35:43 -0500

    final changes for Recipe Service and Components

--- src/app/recipebook-detail/recipebook-detail.component.ts
+++ src/app/recipebook-detail/recipebook-detail.component.ts
@@ -4,6 +4,8 @@ import { Recipe } from '../recipe.model';
 import { AngularFireStorage } from 'angularfire2/storage';
 import { AngularFireModule } from '@angular/fire';
 import { AuthService} from '../auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../shared/services/user';
 
 
 @Component({
@@ -13,9 +15,28 @@ import { AuthService} from '../auth.service';
 })
 export class RecipebookDetailComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[]
+  constructor(private recipeService: RecipeService, public authService: AuthService, private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  recipe: Recipe;
+  user: User;
+
+  getRecipe(): void {
+    this.recipeService.getUserRecipe(this.recipe.id, this.user.uid).subscribe(
+      r => this.recipe = r
+    );
   }
+  ngOnInit() {
 
+    this.authService.afAuth.authState.subscribe( userdata => {
+      if (userdata) { this.user = userdata };
+      this.route.params.subscribe(param => {
+        this.recipeService.getUserRecipe(param["id"], this.user.uid)
+        .subscribe(r => (this.recipe = r)
+  
+        )
+      })
+    });
+
+  }
 }
