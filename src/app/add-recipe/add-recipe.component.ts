a1c45dd60e5299da4df12b95f0f50a763d154eda
shasta <shastavogler@gmail.com>
2020-04-24 21:57:51 -0700

    Created recipeadd component
    Created recipeaddcomponent

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -5,8 +5,8 @@ import { AngularFireStorage } from 'angularfire2/storage'
 import { AngularFireModule } from '@angular/fire'
 import { AuthService } from '../auth.service'
 import { Router, ActivatedRoute } from '@angular/router';
-import { User } from 'firebase';
-
+import { User } from '../shared/services/user';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipe',
@@ -16,12 +16,20 @@ import { User } from 'firebase';
 export class AddRecipeComponent implements OnInit {
 
   newRecipe: Recipe = new Recipe();
+  user: User;
 
+  addRecipe(){
+    this.recipeService.addRecipe(this.newRecipe, this.user.uid).subscribe(
+      r =>
+      this.router.navigate(["savedrecipes/" + r]));
+  }
 
-
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private router: Router, public authService: AuthService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.authService.afAuth.authState.subscribe( userdata => {
+      if (userdata) { this.user = userdata };
+    });
   }
 
 }
