b246f843e1a7b2885ba52ebcd3da1b7903cf8749
crooksdmrs <david.crooks@daveramsey.com>
2020-05-03 13:23:41 -0500

    more add page changes

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from 'src/app/recipe.service';
 import { Recipe } from '../recipe.model';
-import { AngularFireStorage } from 'angularfire2/storage'
-import { AngularFireModule } from '@angular/fire'
-import { AuthService } from '../auth.service'
+import { AngularFireStorage } from 'angularfire2/storage';
+import { AngularFireModule } from '@angular/fire';
+import { AuthService } from '../auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { User } from '../shared/services/user';
 import { FormsModule, FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
-
-
 import { Location } from '@angular/common';
 
 @Component({
@@ -16,6 +14,7 @@ import { Location } from '@angular/common';
   templateUrl: './add-recipe.component.html',
   styleUrls: ['./add-recipe.component.css']
 })
+
 export class AddRecipeComponent implements OnInit {
 
   newRecipe: FormGroup;
@@ -23,10 +22,10 @@ export class AddRecipeComponent implements OnInit {
 
   constructor(private recipeService: RecipeService, private router: Router, public authService: AuthService, private route: ActivatedRoute, private location: Location, private fb: FormBuilder) { }
 
-  addRecipe(){
+  addRecipe() {
     this.recipeService.addRecipe(this.newRecipe.value, this.user.uid).subscribe(
       r =>
-      this.router.navigate(["savedrecipes/" + r]));
+        this.router.navigate(["savedrecipes/" + r]));
   };
 
   backClicked() {
@@ -45,30 +44,27 @@ export class AddRecipeComponent implements OnInit {
     return this.newRecipe.get('ingredients') as FormArray
   }
 
-////////////
+  addInstruction() {
+    this.instructionForms.push(new FormControl(''));
+  }
 
-addInstruction() {
-  this.instructionForms.push(new FormControl(''));
-}
+  deleteInstruction() {
+    let length = this.instructionForms.length
+    this.instructionForms.removeAt(length - 1)
+  }
 
-deleteInstruction() {
-  let length = this.instructionForms.length
-  this.instructionForms.removeAt(length-1)
-}
-
-get instructionForms() {
-  return this.newRecipe.get('instructions') as FormArray
-}
+  get instructionForms() {
+    return this.newRecipe.get('instructions') as FormArray
+  }
 
   ngOnInit(): void {
-    this.authService.afAuth.authState.subscribe( userdata => {
+    this.authService.afAuth.authState.subscribe(userdata => {
       if (userdata) { this.user = userdata };
     });
 
+    let date = new Date;
 
-     let date = new Date;
-
-      this.newRecipe = this.fb.group({
+    this.newRecipe = this.fb.group({
       name: "",
       desc: "",
       ingredients: this.fb.array([""]),
@@ -78,9 +74,6 @@ get instructionForms() {
       servingSize: "",
       imageLink: "",
       timeStamp: date
-
-
-     
     })
 
   }
